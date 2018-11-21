4a5f5f4cfc250b9b3ce2d849c6090b75e4c07952
Dario Elbadry <elbadry.d@gmail.com>
2018-11-20 20:25:55 -0600

    created origin and dest fields

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
     <p>Hello Next.js blah de fdsfsdfsd</p>
+    <Link href='/start'><a>Start</a></Link>
   </div>
 ) 
 
-export default Index 
+export default Index
